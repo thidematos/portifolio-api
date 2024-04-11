be5d4df5f32e80d36d370a77fcb0c3eba49211a7
Thiago Luiz de Matos <127130121+thidematos@users.noreply.github.com>
2024-04-11 01:50:33 -0300

    Implemented CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const workRouter = require('./routers/workRouter');
 
 const app = express();
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.json());
 
 app.use(morgan('dev'));
+
+app.use(cors());
 
 app.use(express.static('./public'));
 
